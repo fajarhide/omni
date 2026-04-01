cffbe415b5f6d0dd86e9fa8e33d518d64d266ed6
fajarhide <fajarhide@gmail.com>
2026-04-02 00:30:27 +0700

    chore: update formula SHA-256 for v0.5.4-rc5

--- omni.rb
+++ omni.rb
@@ -1,28 +1,28 @@
 class Omni < Formula
   desc "Semantic Signal Engine — Less noise. More signal. Right signal."
   homepage "https://github.com/fajarhide/omni"
-  version "0.5.4-rc4"
+  version "0.5.4-rc5"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/fajarhide/omni/releases/download/v#{version}/omni-v#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "0c4e120bcd6a3c28f094e63f00bea456af745648558c9913978302e64e4061cd"
+      sha256 "09f7b81a737a907508e32ee354004966733eefa75d8e3f71eda38308b2bf4a8a"
     end
     on_intel do
       url "https://github.com/fajarhide/omni/releases/download/v#{version}/omni-v#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "a315b1d761e8cbc334a7e393d3ffcb6249311584555a56b8165ba60d08fdaf3b"
+      sha256 "58ed424c65b0ba4ba92edf5295b8ca9976f891b7c98f024e6aef5cabe7826678"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/fajarhide/omni/releases/download/v#{version}/omni-v#{version}-aarch64-unknown-linux-musl.tar.gz"
-      sha256 "36948a8ac455962f523bc63c5be908558e7d0fef19aefeb300e560331a2730fb"
+      sha256 "1b4bdc956d415e1ebfb007df336a855b036a146d5f520e339204be9f2fb637d6"
     end
     on_intel do
       url "https://github.com/fajarhide/omni/releases/download/v#{version}/omni-v#{version}-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "0f7da2d67a68c7c97cab2c022a4b11b3efa9fdbc1090265c8e754ea379b48b01"
+      sha256 "efba57107e6328e5cb3f1d9c0bedac1f59cfa39f8b4f24b53b61933dc68c024a"
     end
   end
 
